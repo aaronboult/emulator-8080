83c48457595b96b98b0f28719d44000568b0bf03
Aaron Boult <aaronboult25@gmail.com>
2020-06-06 16:31:47 +0100

    Updated README, added icon to built executable and removed SDL2 Image dependency

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+extern crate winres;
+
 use std::env;
 use std::path::PathBuf;
 
@@ -38,5 +40,11 @@ fn main() {
                 }
             }
         }
+    }
+
+    if cfg!(target_os = "windows") {
+      let mut res = winres::WindowsResource::new();
+      res.set_icon("icon.ico");
+      res.compile().unwrap();
     }
 }
